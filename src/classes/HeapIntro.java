65f341a9fc7bd1962017afc43e577ca2224a9778
ikostan <superegorius@gmail.com>
2017-09-01 16:50:31 -0700

    StackIntro: using common stack methods

--- src/classes/HeapIntro.java
+++ src/classes/HeapIntro.java
@@ -3,7 +3,7 @@ package classes;
 public class HeapIntro {
 
 	private final String TAG = HeapIntro.class.getSimpleName();
-
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/classes/StackIntro.java
+++ src/classes/StackIntro.java
@@ -1,13 +1,43 @@
 package classes;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class StackIntro {
 
 	private final String TAG = StackIntro.class.getSimpleName();
 
+	private final static int MAX = 25;
+	private static Random rnd;
+	private static Stack<Integer> stack;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		rnd = new Random();
+		stack = new Stack<>();
+		
+		while(stack.size() < MAX){			
+			stack.push(rnd.nextInt(MAX));
+		}
+		
+		int i = 0;
+		
+		while(i < stack.size()){
+			
+			System.out.println(stack.peek());
+			i++;
+		}
+		System.out.println("Size: " + stack.size());
+		
+		
+		while(!stack.empty()){
+			
+			System.out.println(stack.pop());
+		}
+		System.out.println("Size: " + stack.size());
+		
 	}
 	
+	
+	//END
 }
