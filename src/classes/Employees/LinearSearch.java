5bd0265632db25d08fff5cd1906fb742a8e46c00
ikostan <superegorius@gmail.com>
2017-09-01 16:16:10 -0700

    LinearSearch

--- src/classes/Employees/LinearSearch.java
+++ src/classes/Employees/LinearSearch.java
@@ -1,7 +1,46 @@
 package classes.Employees;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class LinearSearch {
 
 	private final String TAG = LinearSearch.class.getSimpleName();
+	private static List<Integer> list;
+	private static Random rnd;
+	private final static int MAX = 25;
 	
+	public static void main(String[] args){
+		
+		list = new ArrayList<>();
+		rnd = new Random();
+		
+		while(list.size() < MAX){
+			
+			int num = rnd.nextInt(MAX);
+			
+			if(!list.contains(num)){
+				
+				list.add(num);
+			}
+		}
+		
+		for(Integer intr : list){
+			
+			System.out.println(intr);
+		}
+		
+		int number = rnd.nextInt(MAX);
+		
+		for(int index = 0; index < list.size(); index++){
+			
+			if(list.get(index) == number){
+				
+				System.out.println(String.format("number %d has index %d", number, index));
+			}
+		}
+		
+	}
 }
