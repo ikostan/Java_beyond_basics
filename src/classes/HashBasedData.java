14d371fb3feb3519d116bf98cc57f668bd2b82db
ikostan <superegorius@gmail.com>
2017-09-01 17:57:14 -0700

    HashBasedData

---
+++ src/classes/HashBasedData.java
@@ -0,0 +1,17 @@
+package classes;
+
+import java.util.Random;
+
+public class HashBasedData {
+
+	private final String TAG = HashBasedData.class.getSimpleName();
+	
+	private final static int MAX = 1000;
+	private static Random rnd;
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
