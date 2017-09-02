e17f6a194b8b5e403d967fce596c5c9da9131c2b
ikostan <superegorius@gmail.com>
2017-09-01 19:39:11 -0700

    SetIntro: add, remove, find

--- src/classes/SetsIntro.java
+++ src/classes/SetsIntro.java
@@ -1,6 +1,7 @@
 package classes;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
 
@@ -18,14 +19,61 @@ public class SetsIntro {
 		set = new HashSet<Object>();
 		
 		setHashSet();
-		
+		printAll();
+		testIsExist(rnd.nextInt(MAX));
+		removeItem(rnd.nextInt(MAX));
+		removeAll();
+		printAll();
 	}
 
 	
+	private static void removeItem(int nextInt) {
+		
+		if(set.contains(nextInt)){
+			
+			System.out.println("removing: " + nextInt);
+			set.remove(nextInt);
+		}
+		else{
+			
+			System.out.println("Can not remove the item. Set does not contain: " + nextInt);
+		}
+	}
+
+
+	private static void removeAll() {
+
+		set.removeAll(set);
+	}
+
+
 	private static void setHashSet(){
 		
-		for(int i = 0; i < MAX / 10; i++){		
+		while(set.size() < MAX /10){		
 			set.add(rnd.nextInt(MAX));
+		}
+	}
+	
+	
+	private static void printAll(){
+		
+		Iterator iterator = set.iterator();
+		while(iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
+		System.out.println("Size: " + set.size());
+	}
+	
+	
+	private static void testIsExist(Integer intr){
+		
+		if(set.contains(intr)){
+			
+			System.out.println("Set contains: " + intr);
+		}
+		else{
+			
+			System.out.println("Set does not contain: " + intr);
 		}
 	}
 	
